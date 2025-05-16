1cc6a3c1a469b71f685042d8af8439da611c6a09
Monkey D Luffy <bredun.denis@gmail.com>
2025-05-16 15:54:15 +0300

    fix: import WeatherModule into SubscriptionModule to resolve WeatherService dependency

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscription } from './entities/subscription.entity';
 import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
+import { WeatherModule } from '../weather/weather.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Subscription])],
+  imports: [TypeOrmModule.forFeature([Subscription]), WeatherModule],
   providers: [SubscriptionService],
   controllers: [SubscriptionController],
 })
